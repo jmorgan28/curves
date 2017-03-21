55e4c739d3d56eaa674e756272d7e7158dedf825
Jackson Morgan <jacksonmorgan@Jacksons-MacBook-Pro.local>
2017-03-21 00:17:54 -0400

    curves are done

--- main.c
+++ main.c
@@ -41,7 +41,7 @@ int main(int argc, char **argv) {
   print_matrix(generate_curve_coefs(1,2,3,4,0)); 
   add_circle(edges, 250, 250, 0, 200, 0);
   add_circle(edges, 175, 325, 0, 50, 0);
-  add_curve(edges,150,150,350,150,0,-100,0,150,0, 1);
+  add_curve(edges,150,150,350,150,-100,-100,100,150,0, 1);
   add_curve(edges,200,250,150,50,300,250,300,250,0, 0);
   draw_lines(edges,s,c);
   
